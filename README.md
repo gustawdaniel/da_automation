99c8d22f7fa6e004b4883452e850d81ff40c78e3
Daniel Gustaw <gustaw.daniel@gmail.com>
2018-08-25 05:26:56 +0200

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # da_automation
+
+Run PHP server
+
+    php -S localhost:8000
+
+And in second terminal
+
+    firefox localhost:8000 &
---
+++ js/custom.js
@@ -0,0 +1,257 @@
+// Ready translated locale messages
+const messages = {
+    en: {
+        header: {
+            l1: 'Automate',
+            l2: 'arranging appointments',
+            l3: 'from',
+            l4: 'dating sites',
+            sub: 'Subscribe',
+            news: 'to our newsletter for fast updates & news',
+            email: 'Your Email Address...'
+        },
+        features: {
+            header: 'Features',
+            list: [
+                {
+                    header: 'Time Saving',
+                    paragraph: 'Save your precious time to use it real meetings'
+                },
+                {
+                    header: 'Real Meetings',
+                    paragraph: 'Omit looking at avatars and have fun from live meetings'
+                },
+                {
+                    header: 'Photos Improvements',
+                    paragraph: 'Get advices from our experts about making best photos'
+                },
+                {
+                    header: 'Machine Learning',
+                    paragraph: 'Use artifical intellience to flirt by internet'
+                }
+            ]
+        },
+        why: {
+            header: 'Why chose us',
+            first: {
+                header: 'Meet real people',
+                paragraph: 'Research shows that live communication allows you to create a deeper relationship much faster than writing, and it is more enjoyable :)'
+            },
+            second: {
+                header: 'No waste time',
+                paragraph: '44% of users aged 18 to 22 use Tinder to overcome self-confidence, you do not have to waste time on such people'
+            },
+            third: {
+                header: 'Expert advice',
+                paragraph: 'As part of the service you get advice on the preparation of the bedroom, fashionable hairstyles, but also topics for conversation or lowering the voice'
+            }
+        },
+        testimonials: {
+            header: 'Testimonials',
+            list: [
+                {
+                    paragraph: 'When I started using dating apps, my goal was to meet cool women, not to click pictures and sit with my nose on the phone. Thanks to auto-dating.pl, using dating services again makes sense to me.',
+                    name: 'Beau',
+                    age: '22'
+                },
+                {
+                    paragraph: 'I had moderate results before using the site. It happened to arrange a meeting, but I always felt that much of the conversation did not lead to anything constructive. Now I focus only on relationships with women who want to meet with me, and live a much deeper relationship easily. I am more satisfied with my social life, because instead of writing with strangers, I can meet real friends.',
+                    name: 'Luke',
+                    age: '27'
+                },
+                {
+                    paragraph: 'Since I remember, I have valued my time very much. Dating sites were supposed to help in dating interesting people, but I had the impression that people using them want to write endlessly instead of meeting and after 15 minutes to know if there is chemistry or not. The autodating.pl service does what dormant websites should allow, but it does not require losing time on inconvenient conversations.',
+                    name: 'Ricky',
+                    age: '25'
+                }
+            ]
+        },
+        pricing: {
+            header: 'Pricing',
+            buy: 'Purchase',
+            list: [
+                {
+                    name: 'Basic',
+                    price: '$5',
+                    up: 'Up To 1,500 clicks / month',
+                    photo: 'One improved photo',
+                    stats: 'Access to basics statistics',
+                },{
+                    name: 'Standard',
+                    price: '$15',
+                    up: 'Up To 5,000 clicks / month',
+                    photo: 'Two improved photos',
+                    stats: 'Stats in comparison typical results',
+                    open: 'Possibility of creating your openers',
+                },{
+                    name: 'Advance',
+                    price: '$35',
+                    up: 'Up To 12,000 clicks / month',
+                    photo: 'Five improved photos',
+                    stats: 'Detailed statistics about all clicks',
+                    open: 'Private, secret, unique openers',
+                    bonuses: 'Faster access to our training materials'
+                },
+            ]
+        },
+        available: {
+            header: 'Focus on experiencing',
+            paragraph: 'With us you will never feel the frustration associated with performing repetitive activities to arrange an appointment via the Internet. Pay attention to life, not the phone.'
+        },
+        faq: {
+            header: 'FAQ',
+            list: [
+                {
+                    q: 'How it works?',
+                    a: 'Our cooperation requires you to give a name, a characteristic character of the style of being, to send a few photos, to specify free hours for meetings and places. Our scope is to create accounts for you on appropriate dating sites and to make conversations. Hangouts will be added to your calendar and you will receive notifications associated with them.'
+                },{
+                    q: 'How do you make meetings so cheap?',
+                    a: 'All thanks to automation. Conversations are conducted by our expert only when exploring new dialog trees. Thanks to the incredible progress that has been made in this field, it is increasingly difficult to distinguish between writing and the machine from writing with a human, and our algorithm has to learn to effectively arrange encounters of experience gathered on all conversations of all clients.'
+                },{
+                    q: 'How can I influence the conversation?',
+                    a: 'In the case of standard and advanced subscriptions, these are special starts. In addition to characterizing your lifestyle, you allow us to adapt the vocabulary to it. In addition, there is little point in focusing on this, because the real relationship starts only at the meeting, writing the text should be treated only as a filter, and not a tool for building relationships.'
+                },{
+                    q: 'Is bot spraying ethical?',
+                    a: 'It all depends on what is your intention. If it is to share what is good with another person, then our platform can be treated like a car - it accelerates reaching a specific point. We believe that sharing a smile face-to-face is good, and stare at the phone does not, that\'s why we are proud that we can create and use this platform ourselves.'
+                },
+            ]
+        }
+    },
+    pl: {
+        header: {
+            l1: 'Zautomatyzuj',
+            l2: 'umawianie spotkań',
+            l3: 'z serwisów',
+            l4: 'randokwych',
+            sub: 'Zapisz się',
+            news: 'do naszego newslettera, aby otrzymywać szybkie aktualizacje i wiadomości',
+            email: 'Twój Adres Email...'
+        },
+        features: {
+            header: 'Zalety',
+            list: [
+                {
+                    header: 'Oszczędzasz czas',
+                    paragraph: 'Oszczędź swój cenny czas na klikaniu i wykorzystaj go na spotkania'
+                },
+                {
+                    header: 'Realne spotkania',
+                    paragraph: 'Omiń patrzenie na awatary i ciesz się przebywaniem z żywą osobą'
+                },
+                {
+                    header: 'Ulepszanie zdjęć',
+                    paragraph: 'Otrzymaj rady naszych ekspertów w temacie doboru najlepszych zdjęć'
+                },
+                {
+                    header: 'Uczenie maszynowe',
+                    paragraph: 'Używaj sztucznej inteligencji do flirtowania przez internet'
+                }
+            ]
+        },
+        why: {
+            header: 'Dlaczego wybierasz nas',
+            first: {
+                header: 'Spotykasz żywe osoby',
+                paragraph: 'Badania wykazują, że komunikacja na żywo pozwala dużo szybciej wytworzyć głęboką więź niż pisanie, a do tego jest przyjemniejsza :)'
+            },
+            second: {
+                header: 'Nie tracisz czasu',
+                paragraph: '44% użytkowników w wieku 18 do 22 lat korzysta z Tindera do pdobijania pewności siebie, nie musisz marnować czasu na takie osoby'
+            },
+            third: {
+                header: 'Doradztwo eksperta',
+                paragraph: 'W ramach usługi dostajesz porady na temat przygotowania sypialni, modnych fryzur, ale też tematów do rozmowy czy obniżania głosu'
+            }
+        },
+        testimonials: {
+            header: 'Rekomendacje',
+            list: [
+                {
+                    paragraph: 'Kiedy zacząłem korzystać z aplikacji randkowych, moim celem było poznawanie fajnych kobiet, a nie klikanie w zdjęcia i siedzenie z nosem w telefonie. Dzięki auto-dating.pl używanie serwisów randkowych ponownie ma dla mnie sens.',
+                    name: 'Beau',
+                    age: '22'
+                },
+                {
+                    paragraph: 'Przed skorzystaniem z serwisu miałem umiarkowane rezultaty. Zdarzało się umówić na spotkanie, ale zawsze czułem, że znaczna część rozmów nie prowadzi do niczego konstruktywnego. Teraz skupiam się tylko na relacjach z kobietami, które chcą się ze mną spotkać, a na żywo łatwo nawiązać znacznie głębszą relację. Jestem bardziej zadowolony z mojego życia towarzyskiego, bo zamiast pisania z nieznajomymi kobietami mogę spotkać się z prawdziwymi znajomymi.',
+                    name: 'Luke',
+                    age: '27'
+                },
+                {
+                    paragraph: 'Odkąd pamiętam bardzo ceniłem swój czas. Portale randkowe miały w założeniu pomóc w umawianiu się z ciekawymi ludźmi, ale odnosiłem wrażenie, że osoby z nich korzystające chcą pisać w nieskończoność zamiast się spotkać i po 15 minutach wiedzieć, czy jest chemia czy nie. Serwis autodating.pl robi to co powinny umożliwiać serwisy randkowe, ale nie wymaga tracenia czasu na nie prowadzące do nikąd konwersacje.',
+                    name: 'Ricky',
+                    age: '25'
+                }
+            ]
+        },
+        pricing: {
+            header: 'Cennik',
+            buy: 'Kupno',
+            list: [
+                {
+                    name: 'Podstawowy',
+                    price: '18.40 zł',
+                    up: 'Do 1,500 kliknięć miesięcznie',
+                    photo: 'Jedno poprawione zdjęcie',
+                    stats: 'Dostęp do podstawowych statystyk',
+                },{
+                    name: 'Standardowy',
+                    price: '55.20 zł',
+                    up: 'Do 5,000 kliknięć miesięcznie',
+                    photo: 'Dwa poprawione zdjęcia',
+                    stats: 'Statystyki w stosunku do średnich wyników',
+                    open: 'Możliwość tworzenia własnych otwarć',
+                },{
+                    name: 'Premium',
+                    price: '128.80 zł',
+                    up: 'Do 12,000 kliknięć miesięcznie',
+                    photo: 'Pięć poprawionych zdjęć',
+                    stats: 'Szczegółowe statystyki każdego kliknięcia',
+                    open: 'Prywatne, tajne unikalne otwarcia rozmów',
+                    bonuses: 'Szybszy dostęp do naszych materiałów'
+                },
+            ]
+        },
+        available: {
+            header: 'Skoncentruj się na przeżywaniu',
+            paragraph: 'Z nami już nigdy nie odczujesz frustracji związanej z wykonywaniem powtarzalnych czynności w celu umówienia spotkania przez internet. Zwróć swoją uwagę w stronę życia, a nie telefonu.'
+        },
+        faq: {
+            header: 'Częste pytania',
+            list: [
+                {
+                    q: 'Jak to działa?',
+                    a: 'Nasza współpraca wymaga podania przez Ciebie imienia, kródkiej charakterystyki towjego stylu bycia, wysłania kilku zdjęć, określenia wolnych godzin na spotkania i miejsca. W naszym zakresie jest założenie Ci kont w stosownych serwisach randkowych i wykonywanie konwersacji. Umówione spotkania będą dodawane do Twojego kalendarza i będziesz otrzymywał powiadomienia z nimi związane.'
+                },{
+                    q: 'W jaki sposób umawiacie spotkania tak tanio?',
+                    a: 'Wszystko dzięki automatyzacji. Rozmowy prowadzone są przez naszego eksperta jedynie w przypadku eksploracji nowych drzew dialogowych. Dzięki niesamowitemu postępowi który dokonał się w tej dziedzinie coraz trudniej odróżnić pisanie z maszyną od pisania z człowiekiem, a nasz algorytm ma do nauki skutecznego umawiania spotkań doświadczenia zebrane na wszyskich konwersacjach wszystkich klientów.'
+                },{
+                    q: 'Jak mogę wpływać na konwersację?',
+                    a: 'W przpypadku pakietów standard oraz premium są to specjalne rozpoczęcia. Poza tym charakteryzując swój styl bycia pozwalasz nam na dostosowanie do niego odpowiedniego słownictwa. Poza tym raczej nie ma sensu skupiać się na tym, ponieważ prawdziwa relacja zaczyna się dopiero na spotkaniu, pisanie tekstu traktować należy jedynie jako filter, a nie narzędzie do budowania więzi.'
+                },{
+                    q: 'Czy sosowanie bota jest etyczne?',
+                    a: 'Wszystko zależy od tego co jest Twoją intencją. Jeśli jest nią dzielenie się tym co dobre z drugą osobą, to naszą platformę można traktować jak samochód - przyśpiesza dotarcie do określonego punktu. Wierzymy, że dzielenie się uśmiechem twarzą w twarz jest dobre, a gapienie w telefon nie, dlatego jesteśmy dumni, że możemy tworzyć i samodzielnie korzystać z tej platformy.'
+                },
+            ]
+        }
+    }
+};
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+    locale: 'en', // set locale
+    messages, // set locale messages
+});
+
+// Now the app has started!
+new Vue({
+    i18n,
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    methods: {
+        changeLangTo(to) {
+            this.$i18n.locale = to;
+        }
+    }
+});
